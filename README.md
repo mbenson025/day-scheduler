0230dc92435db794a3fc62b44ae30807c305c217
mbenson025 <mbenson025@gmail.com>
2022-05-01 17:41:30 -0500

    first commit -setup

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1> Day Scheduler </h1>
+
+## Link
+
+<a href="https://mbenson025.github.io/day-scheduler/">Day Scheduler</a>
---
+++ script.js
@@ -0,0 +1,18 @@
+//global variables
+var now = moment();
+//current day and time
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+
+
+
+//functions
+
+
+
+
+
+
+
+
+//eventListeners
