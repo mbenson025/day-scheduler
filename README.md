a7de4d219b7873f4753160b56645f259601b84c1
mbenson025 <mbenson025@gmail.com>
2022-05-03 11:37:52 -0500

    added icon transition for save click

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Link
 
 <a href="https://mbenson025.github.io/day-scheduler/">Day Scheduler</a>
+
+Credits:
+
+Event Listener for changing an icon: https://stackoverflow.com/questions/45494301/changing-font-awesome-icon-onclick-function
+
+Jquery for changing icon:
+https://stackoverflow.com/questions/15707194/changing-button-icon-upon-onclick-when-using-javascript-bootstrap
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //global variables
 var now = moment();
+
+
 //current day and time
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
@@ -15,4 +17,20 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 
 
-//eventListeners
+
+
+
+$('button').on('click', iconEffect);
+
+function iconEffect(e) {
+  $(this).find('.fa').removeClass('fa-folder-plus').addClass('fa-check');
+  setTimeout(() => {
+    $(this).find('.fa').removeClass('fa-check').addClass('fa-folder-plus');
+  }, 500);
+}
+
+
+//toggle function alternate-
+// function iconSwitch(e) {
+//   $(this).find('.fa').toggleClass('fa-unlock-alt fa-lock');
+// }
