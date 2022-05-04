35a053d80fc84cc2eab0c1af6cf3cfd6e56d3ea1
mbenson025 <mbenson025@gmail.com>
2022-05-04 16:32:54 -0500

    bstrap sizing and localstorage input for save btn

--- README.md
+++ README.md
@@ -6,7 +6,14 @@
 
 Credits:
 
-Event Listener for changing an icon: https://stackoverflow.com/questions/45494301/changing-font-awesome-icon-onclick-function
+<!-- jquery was a much easier solution than this -->
+<!-- Event Listener for changing an icon: https://stackoverflow.com/questions/45494301/changing-font-awesome-icon-onclick-function -->
 
-Jquery for changing icon:
+jquery for changing icon:
 https://stackoverflow.com/questions/15707194/changing-button-icon-upon-onclick-when-using-javascript-bootstrap
+
+Row/column sizing assist(col md-1/col-md-10):
+askBCS support session 147110
+
+localStorage key and value:
+https://stackoverflow.com/questions/40791207/setting-and-getting-localstorage-with-jquery
--- script.js
+++ script.js
@@ -5,32 +5,41 @@ var now = moment();
 //current day and time
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
-
+// var hour = ['9', '10', '11', '12', '1', '2', '3', '4', '5'];
 
 
 //functions
 
+//return from storage
+function getSchedule() {
+  // localStorage.getItem(k);
+  // $(this)
+}
 
 
+// $('.description').val(localStorage.description);
 
-
-
-
-
-
-
-
-$('button').on('click', iconEffect);
-
-function iconEffect(e) {
+$('button').on('click', saveSchedule);
+//icon change
+function setSchedule(e) {
   $(this).find('.fa').removeClass('fa-folder-plus').addClass('fa-check');
+  //icon disappears after time duration
   setTimeout(() => {
     $(this).find('.fa').removeClass('fa-check').addClass('fa-folder-plus');
   }, 500);
+    
+    //save input to localStorage (key + value)
+    let k = $(this).parent().children('textarea').attr('id');
+    let v = $(this).parent().children('.description').val();
+
+
+    localStorage.setItem(k, v);
+  
 }
+
 
 
 //toggle function alternate-
 // function iconSwitch(e) {
-//   $(this).find('.fa').toggleClass('fa-unlock-alt fa-lock');
+//   $(this).find('.fa').toggleClass('fa-folder-plus fa-check');
 // }
